9997346dc29dbc699009e6c92d35e2077bcbba0d
xXxAleXxX01 <61098010+xXxAleXxX01@users.noreply.github.com>
2020-05-14 17:19:00 +0300

    Update __resource.lua

--- __resource.lua
+++ __resource.lua
@@ -1,3 +1,10 @@
+--------------------------CREDITS--------------------------
+-------------------Script made by AleXxX-------------------
+--      Script made for ZenTrix Romania RolePlay         --
+--         Discord: https://discord.gg/BEFrqay           --
+--   Copyright 2019 ©AleXxXScript's. All rights served   --
+-----------------------------------------------------------
+
 
 server_script {
     "@vrp/lib/utils.lua",
